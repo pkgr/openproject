d2a9f0edd69f668bee9eb4b9afc9d42ccdeaaa09
Philipp Tessenow <philipp@tessenow.org>
2014-05-14 12:01:23 +0200

    fetch bower assets on pkgr.io
    * add npm and node as a build dependency
    * npm install bower and bower install

--- packaging/debian/setup.sh
+++ packaging/debian/setup.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
 # we need postgresql for asset precompilation
 # see: https://pkgr.io/doc
 sudo service postgresql start
+
+npm install bower
+nodejs node_modules/bower/bin/bower install
+
